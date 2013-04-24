cfd1bf3231346d51a08fcae466d07abb29314891
rhelmstetter <Rachel.helmstetter@gmail.com>
2013-04-23 23:52:30 -0700

    Added Library.csproj

---
+++ Assignment 5/Library/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Pet
+    {
+        int age;
+        double price;
+    }
+
+    public class Dog : Pet
+    {
+        string breed;
+    }
+
+    public class Cat : Pet
+    {
+        string color;
+        string breek;
+    }
+
+    public class Bird : Pet
+    {
+        string type;
+    }
+}
