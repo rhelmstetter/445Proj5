4a005dfcf5980667067a83dd80d796c1744352ad
rhelmstetter <Rachel.helmstetter@gmail.com>
2013-04-27 14:22:42 -0700

    Getters and Setters

---
+++ Assignment 5/WebStore/CurrentUser.ascx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class CurrentUser : System.Web.UI.UserControl
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        String user;
+        user = "Guest";
+        // if (someone logged in)
+        // set user to someone else
+        lblUser.Text = "Hello, " + user;
+    }
+}
