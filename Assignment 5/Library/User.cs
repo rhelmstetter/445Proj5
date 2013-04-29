c95fa385ffc14534380c1bd8a10ea3b96c4509e5
Dan <dhankins@asu.edu>
2013-04-28 18:31:30 -0700

    j

--- Assignment 5/Library/User.cs
+++ Assignment 5/Library/User.cs
@@ -63,10 +63,11 @@ namespace Library
 
     public class UserDao
     {
-       
+        private string path = "c:\\Users.txt";
 
         public void addUser(User newUser)
         {
+
             string path = "c:\\Users.txt";
 
             // Load all users
@@ -74,13 +75,20 @@ namespace Library
             {
                 string users = File.ReadAllText(path);
                 users = users + newUser.toString() + "\n";
-                File.WriteAllText(users, path);
+
+                //File.WriteAllText(users, @path);
+
+                System.IO.StreamWriter output = new System.IO.StreamWriter(@"C:\\Users.txt");
+                output.WriteLine(users);
+                output.Close();
+
             }
             catch (Exception e)
             {
                 Debug.WriteLine(e.Message);
                 Debug.WriteLine(e.GetType());
             }
+
         }
 
         public User loginUser(string username, string password)
@@ -116,7 +124,6 @@ namespace Library
 
         public User getUser(string username)
         {
-            string path = "C:\\Users.txt";
 
             try
             {
